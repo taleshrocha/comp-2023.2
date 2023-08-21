db903c543a67020fcd33735cd2f339c61a16bb99
JohnVithor <jv.venceslau.c@gmail.com>
2023-08-21 10:33:02 -0300

    WIP

---
+++ testes/defines.h
@@ -0,0 +1,53 @@
+
+#define ID 10000  //Identifier token
+#define CONST 20000
+#define T_INT 300
+#define T_REAL 400
+#define T_CHAR 500
+#define T_BOOL 600
+
+#define V_INT 700
+#define V_CHAR 800
+#define V_BOOL 900
+#define V_REAL 1000
+
+#define EQ 1100
+#define LEQ 1200
+#define GEQ 1300
+#define GE 1400
+#define LE 1500
+#define NEQ 1600
+
+#define PLUS 1700
+#define MINUS 1800
+#define DIVIDE 1900
+#define MULTIPLY 2000
+#define MOD 2100
+
+#define LOOP 2200
+#define IF 2300
+#define ELSE 2400
+#define ELSIF 2500
+#define _BEGIN 2600
+#define END 2700
+
+#define AND 2800
+#define NOT 2900
+#define OR 3000
+
+#define TYPE 123131
+#define ARRAY 12312332
+#define EXIT 1231233
+#define WHEN 412434
+#define TO 115345
+#define STEP 534737
+#define THEN 21256
+#define ATTRIB 41364373
+
+#define FUNCTION 457457
+#define PROCEDURE 2342762
+#define RETURN 23654373
+#define BREAK 2135276
+#define CONTINUE 3576485
+#define RECORD 78423
+#define OF 23236
