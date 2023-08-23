ed355fbd5b1b8e4292286cc72b1aa12c65227104
Daniel Sehn Colao <66954777+1DanielSC@users.noreply.github.com>
2023-08-23 09:37:06 -0300

    Update symbolTable.h

--- include/symbolTable.h
+++ include/symbolTable.h
@@ -1,19 +1,18 @@
 #ifndef SYMBOLTABLE_H
 #define SYMBOLTABLE_H
 
-// TODO: Usar alocação dinamica para o tamanho da tabela
-#define MAXNUMSYMBOLS 100
+#define SYMBOL_TABLE_INITIAL_SIZE 100
 
-// TODO: Mover SymbolEntry e logica da tabela de simbolos para outro arquivo
 typedef struct SymbolEntry {
     char *name;
-    int type;
+    int class;
 } SymbolEntry;
 
-extern SymbolEntry symbol_table[MAXNUMSYMBOLS]; // Tabela de símbolos (tamanho máximo de 100)
+extern SymbolEntry * symbol_table; // Tabela de símbolos (tamanho inicial: 100)
 extern int numSymbols;
 
 void initializeSymbolTable();
 void addSymbol(const char *symbol_name, const int token_class);
+void printFirst10Entries();
 
 #endif // SYMBOLTABLE_H
