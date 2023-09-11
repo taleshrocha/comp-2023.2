7b9d4498c0512524c02a1423034fc91ce0d42fb5
JohnVithor <jv.venceslau.c@gmail.com>
2023-09-11 07:44:35 -0300

    update defines.h

--- include/defines.h
+++ include/defines.h
@@ -1,55 +1,70 @@
 
-#define ID 10000  // Identifier token
-#define CONST 20000
-#define T_INT 300
-#define T_REAL 400
-#define T_CHAR 500
-#define T_BOOL 600
+#define ID 1
+#define CONST 2
+#define T_INT 3
+#define T_REAL 4
+#define T_CHAR 5
+#define T_BOOL 6
 
-#define V_INT 700
-#define V_CHAR 800
-#define V_BOOL 900
-#define V_REAL 1000
-#define V_STRING 1001
+#define V_INT 7
+#define V_CHAR 8
+#define V_BOOL 9
+#define V_REAL 10
+#define V_STRING 11
 
-#define EQ 1100
-#define LEQ 1200
-#define GEQ 1300
-#define GE 1400
-#define LE 1500
-#define NEQ 1600
+#define EQ 12
+#define LEQ 13
+#define GEQ 14
+#define GREATER 15
+#define LESS 16
+#define NEQ 17
 
-#define PLUS 1700
-#define MINUS 1800
-#define DIVIDE 1900
-#define MULTIPLY 2000
-#define MOD 2100
+#define PLUS 18
+#define MINUS 19
+#define DIVIDE 20
+#define MULTIPLY 21
+#define MOD 22
 
-#define LOOP 2200
-#define IF 2300
-#define ELSE 2400
-#define ELSIF 2500
-#define _BEGIN 2600
-#define END 2700
 
-#define AND 2800
-#define NOT 2900
-#define OR 3000
+#define LOOP 23
+#define IF 24
+#define ELSE 25
+//#define ELSIF 26   --- REMOVIDO POR NAO ESTAR NA GRAMATICA
+#define _BEGIN 26
+#define END 27
 
-#define TYPE 123131
-#define ARRAY 12312332
-#define EXIT 1231233
-#define WHEN 412434
-#define TO 115345
-#define STEP 534737
-#define THEN 21256
-#define ATTRIB 41364373
+#define AND 28
+#define NOT 29
+#define OR 30
 
-#define FUNCTION 457457
-#define PROCEDURE 2342762
-#define RETURN 23654373
-#define BREAK 2135276
-#define CONTINUE 3576485
-#define RECORD 78423
-#define OF 23236
-#define VAR 123125
+#define TYPE 31
+#define ARRAY 32
+#define EXIT 33
+#define WHEN 34
+#define TO 35
+#define STEP 36
+#define THEN 37
+#define ATTRIB 38 //Atribuicao
+
+
+#define FUNCTION 39
+#define PROCEDURE 40
+#define RETURN 41
+#define BREAK 42
+#define CONTINUE 43
+#define RECORD 44
+#define OF 45
+#define VAR 46
+
+#define LPAR 47 // (
+#define RPAR 48 // )
+#define LBRA 49 //Left bracket [
+#define RBRA 50 //right bracket ]
+
+#define DOT 51
+#define COMMA 52
+#define SEMICOLON 53
+#define COLON 54
+
+#define FOR 55
+#define INTERVAL 56 //Corresponds to '..'
